f8e0d55274f504599a306618420597e1794dede6
Tonyjiafan <358982833@qq.com>
2017-01-19 11:51:54 +0800

    修改错误database.js

--- controller/Dao/database.js
+++ controller/Dao/database.js
@@ -10,8 +10,8 @@ function getConn(){
         user:"root",
         password:"111111",  //你的mysql root密码
         database:"yohobuy"   //你的额数据库名称
+      })
 }
-
 //查询mysql数据库的方法
 exports.query = function (sql,params,callback) {
     var conn = getConn();
